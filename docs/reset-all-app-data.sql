aa9734d172fecfcdcb47c4d91622052acc0c2562
mmuuemail-ctrl <mm.uu.email@gmail.com>
2026-04-12 14:52:45 +0200

    Create reset-all-app-data.sql

---
+++ docs/reset-all-app-data.sql
@@ -0,0 +1,6 @@
+delete from public.view_events;
+delete from public.alerts;
+delete from public.credit_purchases;
+delete from public.qr_codes;
+delete from public.profiles;
+delete from auth.users;
